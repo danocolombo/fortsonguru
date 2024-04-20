e378fb9883184dd7ebab7b28c7e3d68143fe0cf5
Dano <danocolombo@gmail.com>
2024-04-20 08:19:40 -0400

    using job model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 Route::get('/home', function () {
     return view('home');
@@ -9,12 +9,46 @@ Route::get('/home', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ]
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
+Route::get('jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$10,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$40,000'
+        ]
+    ];
+
+    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
 
     return view('job', ['job' => $job]);
 });
